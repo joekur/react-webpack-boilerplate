768d3969d63f50b77235bd3df4280f3b93c2a599
Joe Kurleto <joekur@reverb.com>
2017-03-03 13:36:37 -0600

    Add css autoprefixer via postcss

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader!sass-loader'
+          use: 'css-loader!postcss-loader!sass-loader'
         })
       }
     ]
